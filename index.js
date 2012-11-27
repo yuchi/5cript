a34b01f7daa4f9d33719253a62098b5642c86f8f
Pier Paolo Ramon <ramonpierre@gmail.com>
2012-11-27 01:52:21 +0100

    Better API

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
-exports.parser = require('./lib/parser');
-exports.render = require('./lib/render');
+module.exports = fivecript;
+
+// TODO Use options!
+function fivecript (str, options) {
+  var document = parser.parse( str );
+  var html = render.html( document, {
+    tablemode: false
+  });
+  return html;
+}
+
+fivecript.parser = require('./lib/parser');
+fivecript.render = require('./lib/render');
 
